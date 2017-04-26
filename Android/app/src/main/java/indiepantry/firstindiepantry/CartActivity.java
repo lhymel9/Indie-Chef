c1aea809ce50d70f3194107bd592617ffa4e3c70
BrandonXHollier <brandon.hollier@gmail.com>
2017-04-26 10:26:39 -0500

    Revert "Revert "Improved Cart Activity, added remove item button""
    This reverts commit a19087ea3b2689de28528ffd310fa0f7b17837c6.

--- Android/app/src/main/java/indiepantry/firstindiepantry/CartActivity.java
+++ Android/app/src/main/java/indiepantry/firstindiepantry/CartActivity.java
@@ -21,9 +21,11 @@ package indiepantry.firstindiepantry;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -31,7 +33,10 @@ import java.util.ArrayList;
 
 public class CartActivity extends AppCompatActivity {
 
-    public static final String EXTRA_ORDER_TOTAL = "indiepantry.firstindiepantry.CART_TOTAL";
+    public static final String EXTRA_ORDER_TOTAL = "CART_TOTAL";
+    public static final String TAG = CartActivity.class.getSimpleName();
+
+    ArrayList<item_display> alCartItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,51 +46,15 @@ public class CartActivity extends AppCompatActivity {
         if (savedInstanceState != null)
             return;
 
-        ScrollView available_items = (ScrollView) findViewById(R.id.cartItemList);
-        LinearLayout verticalLayout = (LinearLayout) findViewById(R.id.cartLinearLayout);
-
         TextView placard = (TextView) findViewById(R.id.cartPagePlacard);
         placard.setText(SideData.getUsername() + "'s Cart");
 
-        ArrayList<item_display> items = SideData.getCart();
-        for(item_display bob: items){
-            LinearLayout horizontalLayout = new LinearLayout(this);
-            horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
-            Button item_button = new Button(this);
-            item_button.setTag(bob);
-            item_button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Button button = (Button) v;
-                    item_display bob = (item_display) button.getTag();
-                    Intent intent = new Intent(CartActivity.this, Item_Activity.class);
-                    SideData.setTemp_item(bob);
-                    startActivity(intent);
-                }
-            });
-            item_button.setText(bob.getName() + " " + bob.getCost());
-
-            Button vendor_button = new Button(this);
-            vendor_button.setTag(bob.getVendor());
-            vendor_button.setText(bob.getVendor_name());
-            vendor_button.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Button button = (Button) v;
-                    Vendor vendor = (Vendor) button.getTag();
-                    Intent intent = new Intent(CartActivity.this,VendorPageActivity.class);
-                    SideData.setTemp_vendor(vendor);
-                    startActivity(intent);
-                }
-            });
-
-            horizontalLayout.addView(item_button);
-            horizontalLayout.addView(vendor_button);
-
-            verticalLayout.addView(horizontalLayout);
-        }
-
-
+        alCartItems = SideData.getCart();
+        ListView lvCartItemList = (ListView) findViewById(R.id.lv_cartItemList);
+        Log.i(TAG,"lvCartItemList assigned");
+        CartItemListAdapter listAdapter = new CartItemListAdapter(this,alCartItems);
+        Log.i(TAG,"listAdapter assigned"+listAdapter);
+        lvCartItemList.setAdapter(listAdapter);
     }
 
     /** Called when the user taps the Checkout button */
